471220e22275ab13ef6e2c432268efc93f6f7dbc
Julia Dickey and Sam Park <julia@>
2016-06-28 13:33:42 -0700

    KILLIN IT

--- lib/numbers_words.rb
+++ lib/numbers_words.rb
@@ -2,12 +2,23 @@
 class Fixnum
   one_to_nineteen = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
 
+  two_digits = { 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety",}
+
   define_method(:translate) do
     if (self.>(0)) && (self.<(20))
       word_small =
       word_small = one_to_nineteen.fetch(self)
+      word_small
+    elsif (self.>=(20)) && (self.<(100))
+      word_medium =
+      number_array = (self.to_s).split("")
+      single_num = number_array.pop().to_i
+      tens_num = number_array.pop().to_i
+      word_small = one_to_nineteen.fetch(single_num)
+      word_med = two_digits.fetch(tens_num)
+      number_word = word_med.concat(word_small)
+
     end
-    word_small
   end
 end
 
--- spec/numbers_words_spec.rb
+++ spec/numbers_words_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#translate')  do
   it("Returns number in word form from one to nineteen")  do
     expect((13).translate()).to(eq("thirteen"))
   end
+  it("Returns number in word form from twenty to twenty-nine")  do
+    expect((27).translate()).to(eq("twentyseven"))
+  end
 end
